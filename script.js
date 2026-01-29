2c8ff93d92c754c6c0f52945a3df18522a6dbcd0
Himanshu Singh <himanshusingh1k@gmail.com>
2026-01-29 18:44:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+const debounced = debounce(console.log, 3000);
+document.body.addEventListener("click", () => debounced(currentTime));
+
 function debounce(callback, delay, immediate = false) {
+  let timeoutId;
+  let currentTime = 0;
+  return function debounced(currentTime) {
+    if (timeoutId) clearTimeout(timeoutId);
+    if (immediate) callback(delay);
+    timeoutId = setTimeout(function () {
+      callback(delay);
+    }, delay);
+  };
+}
   
-  }
-  
-  module.exports = debounce;
+module.exports = debounce;
