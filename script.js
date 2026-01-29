bbc74aeb4690a4a93647029900489204a5d3b4df
Himanshu Singh <himanshusingh1k@gmail.com>
2026-01-29 18:53:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ document.body.addEventListener("click", () => debounced(currentTime));
 
 function debounce(callback, delay, immediate = false) {
   let timeoutId;
-  let currentTime = 0;
-  return function debounced(currentTime) {
-    if (timeoutId) clearTimeout(timeoutId);
-    if (immediate) callback("baz");
-    timeoutId = setTimeout(function () {
-      callback("baz");
-    }, delay);
+  return function debounced(...args) {
+    function doLater() {
+      timeoutId = null;
+      if (!immediate) callback(...args); // Trailing call
+    } // Accept args, pass to callback
+
+    if (immediate && !timeoutId) {
+      callback(...args); // Leading call only if no pending
+      timeoutId = setTimeout(doLater, delay);
+    } else {
+      if (timeoutId) clearTimeout(timeoutId);
+      timeoutId = setTimeout(doLater, delay);
+    }
   };
 }
   
